9e43a960b76e9d41345bda5d6273fc2df11cb806
Heesu Yang <hsge0602@gmail.com>
2019-06-13 11:53:31 +0900

    #2 진행상태바 소스 구현

--- ProgressBar/Form1.cs
+++ ProgressBar/Form1.cs
@@ -11,9 +11,46 @@ namespace ProgressBar
 {
     public partial class Form1 : Form
     {
+        string OrgStr = "";
+        int Num = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblStatus.Text;
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.Num++;
+            if(this.Num > 100)
+            {
+                this.Timer.Enabled = false;
+                return;
+            }
+            this.pbStatus.Value = this.Num;
+            this.lblStatus.Text = this.OrgStr + this.Num + "%";
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = false;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.Num = 0;
+            this.lblStatus.Text = this.OrgStr;
+            this.pbStatus.Value = this.Num;
         }
     }
 }
